9aa2599954633ece8936e89bcf3504c7656daf03
Bamgbelu Adeolu Oreoluwa <adeolubams2007@gmail.com>
2026-07-03 16:28:00 +0100

    Update Day1.sql

--- Days/Day1.sql
+++ Days/Day1.sql
@@ -1,1 +1,46 @@
+```sql
+--	Display only selected columns. 
+SELECT * 
+FROM public.fashion_retail;
 
+--	Count total transactions. 
+SELECT
+	COUNT("Order_ID") AS total_transactions
+FROM public.fashion_retail;
+
+--	Count unique customers. 
+SELECT
+	COUNT(
+		DISTINCT("Customer_ID")) AS total_customers
+FROM public.fashion_retail;
+
+--	Count unique products. 
+SELECT
+	COUNT(
+		DISTINCT("Product_Name")) AS total_unique_products
+FROM public.fashion_retail;
+
+--	List all product categories.
+SELECT "Category"
+FROM public.fashion_retail;
+
+--	List all payment methods. 
+SELECT "Payment_Method"
+FROM public.fashion_retail;
+
+--	Find the earliest order date. 
+SELECT "Order_Date"
+FROM public.fashion_retail
+ORDER BY "Order_Date" ASC
+LIMIT 1;
+
+--	Find the latest order date. 
+SELECT "Order_Date"
+FROM public.fashion_retail
+ORDER BY "Order_Date" DESC
+LIMIT 1;
+--	Count missing values in key columns. 
+SELECT *
+FROM public.fashion_retail
+WHERE "Customer_ID" is null;
+```
